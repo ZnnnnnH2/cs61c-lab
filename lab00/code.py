076ad5bde615f0f0e67bdc52dd1ee93b6872cd75
peter <zhangnianhao@ruc.edu.cn>
2025-08-13 12:44:02 +0800

    fix: code.py

--- lab00/code.py
+++ lab00/code.py
@@ -1,18 +1,29 @@
 def get_airspeed_velocity_of(unladen_swallow):
-  if unladen_swallow.type == "african":
-    return # redacted
-  elif unladen_swallow.type == "european":
-    return # redacted
+	if unladen_swallow.type == "african":
+		return  # redacted
+	elif unladen_swallow.type == "european":
+		return  # redacted
+
 
 # pretend there's code here...
 
 def fizzbuzz(num):
-  if num == 3: # edit this line
-    print(f"{num}: fizz")
-  if num == 5: # edit this line
-    print(f"{num}: buzz")
+	flag1 = False
+	flag2 = False
+
+	if num % 3 == 0:  # edit this line
+		flag1 = True# print(f"{num}: fizz")
+	if num % 5 == 0:  # edit this line
+		flag2 = True# print(f"{num}: buzz")
+
+	if flag1 and flag2:
+		print(f"{num}: fizzbuzz")
+	elif flag1:
+		print(f"{num}: fizz")
+	elif flag2:
+		print(f"{num}: buzz")
 
 for i in range(1, 20):
-  fizzbuzz(i)
+	fizzbuzz(i)
 
 # pretend there's code here...
